8d7c65e17c9dd2a805ded7f6f3471bbf0d7ac1a0
Ekaterina Demakova <demakovaln@mail.ru>
2023-11-06 04:37:57 +0600

    logo

--- SGJ2023/Assets/Code/GameplayLogic/GameQuitter.cs
+++ SGJ2023/Assets/Code/GameplayLogic/GameQuitter.cs
@@ -1,21 +1,41 @@
 ﻿using System.Collections;
+using DG.Tweening;
+using GameplayLogic.Audio;
 using UnityEngine;
+using Zenject;
 
 namespace GameplayLogic
 {
   public class GameQuitter : MonoBehaviour
   {
+    [SerializeField]
+    private CanvasGroup _canvasGroup;
+    
+    [SerializeField, Min(0)]
+    private float _appearDuration = 0.5f;
+    
     [SerializeField, Min(0)]
     private float _quitDuration = 1.5f;
 
-    private void Awake()
+    private IMusicService _music;
+
+    [Inject]
+    private void Construct(IMusicService music)
+    {
+      _music = music;
+    }
+    
+    private void Start()
     {
       StartCoroutine(Quitting());
     }
 
     private IEnumerator Quitting()
     {
-      yield return new WaitForSeconds(_quitDuration);
+      _music.StopMusic();
+      _canvasGroup.DOFade(1, _appearDuration);
+      
+      yield return new WaitForSeconds(_appearDuration + _quitDuration);
       Application.Quit();
     }
   }
