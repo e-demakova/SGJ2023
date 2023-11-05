3829a724f79a34a9df29e390ca3b5b8db0b3f8c0
Ekaterina Demakova <demakovaln@mail.ru>
2023-11-05 21:32:05 +0600

    end credits

---
+++ SGJ2023/Assets/Code/GameplayLogic/GameQuitter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace GameplayLogic
+{
+  public class GameQuitter : MonoBehaviour
+  {
+    [SerializeField, Min(0)]
+    private float _quitDuration = 1.5f;
+
+    private void Awake()
+    {
+      StartCoroutine(Quitting());
+    }
+
+    private IEnumerator Quitting()
+    {
+      yield return new WaitForSeconds(_quitDuration);
+      Application.Quit();
+    }
+  }
+}
