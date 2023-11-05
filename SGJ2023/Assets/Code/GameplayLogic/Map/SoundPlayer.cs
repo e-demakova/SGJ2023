c5711eaaa3c321ab409b3c70f92f678432b3d543
Ekaterina Demakova <demakovaln@mail.ru>
2023-11-06 02:12:48 +0600

    kill animation update

---
+++ SGJ2023/Assets/Code/GameplayLogic/Map/SoundPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace GameplayLogic.Map
+{
+  public class SoundPlayer : MonoBehaviour
+  {
+    [SerializeField]
+    private AudioSource _audio;
+
+    public void Play() =>
+      _audio.Play();
+  }
+}
