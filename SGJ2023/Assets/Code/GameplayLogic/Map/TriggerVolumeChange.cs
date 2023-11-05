949a525048f3325f5fc8f286c21ed438683f4aa8
Ekaterina Demakova <demakovaln@mail.ru>
2023-11-05 18:31:59 +0600

    dead dad art

---
+++ SGJ2023/Assets/Code/GameplayLogic/Map/TriggerVolumeChange.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Observing.GameObjects.Physics;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace GameplayLogic.Map
+{
+  public class TriggerVolumeChange : MonoBehaviour
+  {
+    [SerializeField]
+    private Collider2D _collider;
+    
+    [SerializeField]
+    private VolumeProfile _profile;
+    
+    [SerializeField]
+    private Volume _volume;
+
+    private IDisposable _subscriber;
+
+    private void Awake() =>
+      _subscriber = _collider.TriggerEnter2D().Subscribe(UpdateVolume);
+
+    private void OnDestroy() =>
+      _subscriber.Dispose();
+
+    private void UpdateVolume()
+    {
+      _subscriber.Dispose();
+      _volume.profile = _profile;
+    }
+  }
+}
